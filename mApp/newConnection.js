365827654a7c90c31754e8ea5e32da9ce0e3da60
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-22 17:56:16 +0530

    added insert and update functions

--- mApp/newConnection.js
+++ mApp/newConnection.js
@@ -15,6 +15,49 @@ async function connect() {
         const collections = await db.collections();
         collections.forEach(c => {console.log(c.collectionName);});
         */
+        
+        //------------------------------------------------------------------
+        // find() in dB:
+
+        const student = db.collection("students");
+        const searchCursor = await student.find();
+        // const searchCursor = await student.find({"name": "Akash"});  // to have a more specific search !!
+        
+        const result = await searchCursor.toArray();
+        console.table(result);
+        // result.forEach(r => {console.log(r);})
+        /*
+        while (await searchCursor.hasNext()) {
+            console.log(await searchCursor.next());
+        }
+        */
+
+        //------------------------------------------------------------------
+        // insert() in dB: it is tested and working well !!
+
+        // const insertCursor = await student.insertMany([
+        //     {
+        //         name: "Amit",
+        //         isVerified: true
+        //     },
+        //     {
+        //         name: "Aman",
+        //         isVerified: true
+        //     }
+        // ])
+        // console.log(insertCursor.insertedCount); 
+
+
+        //------------------------------------------------------------------
+        // update() in dB: it is tested and working fine !!
+        /*
+        const updateCursor = await student.updateMany({isVerified: true}, {
+            $set :{courseCount: 4}
+        })
+        console.log(updateCursor.modifiedCount);
+        */
+
+
     }
     catch (ex) {
         console.log("ERROR", ex);
